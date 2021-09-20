cb385412779a91d3bbb1133cf5b6a910d28fea27
Tomas <mrtom@raspberrypi>
2021-09-20 14:25:27 +0100

    asd

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,7 +20,7 @@ def teardown_appcontext(code):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 4044)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
