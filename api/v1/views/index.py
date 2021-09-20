90a5cd461abe62a47d7eeb0a30a5c1ff5cad28bf
Tomas <mrtom@raspberrypi>
2021-09-20 14:19:59 +0100

    asd

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """index.py to connect to API"""
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
 
 @app_views.route('/stats', strict_slashes=False)
 def hbnbStats():
     """hbnbStats"""
-    return '{"status": "OK"}'
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+
 
 if __name__ == "__main__":
     pass
